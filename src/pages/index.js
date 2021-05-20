77969d20779bf998b8207da397b228874eeb47f9
godninoff <godninoff21@yandex.ru>
2021-05-20 22:01:31 +0500

    All feauteall project functions are configured, webpack builds the project, before 1st review

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
-import { initialCards } from './initial-сards.js';
-import {FormValidator}  from './FormValidator.js';
-import {Card} from './Card.js';
-import {PopupWithForm} from './PopupWithForm.js';
-import {PopupWithImage} from './PopupWithImage.js';
-import {Section} from './Section.js';
-import {UserInfo} from './UserInfo.js';
+import { initialCards } from '../components/initial-сards.js';
+import {FormValidator}  from '../components/FormValidator.js';
+import {Card} from '../components/Card.js';
+import {PopupWithForm} from '../components/PopupWithForm.js';
+import {PopupWithImage} from '../components/PopupWithImage.js';
+import {Section} from '../components/Section.js';
+import {UserInfo} from '../components/UserInfo.js';
+import './index.css';
 
 const popupEdit = '.popup_edit';
 const popupAddCard = '.popup_add-card';
